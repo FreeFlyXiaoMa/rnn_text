01d6463fc097a842092e1e6195e51748448294d9
FreeFlyXiaoMa <2429825530@qq.com>
2019-10-07 21:57:53 +0800

    Initial commit

---
+++ rnn.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time     :2019/10/7 21:54
+# @Author   :XiaoMa
+# @Site     :
+# @File     :rnn.py
+# @Software :PyCharm
+
+import tensorflow as tf
+
+#定义超参数
+#num_unroll:展开的步数
+#batch_size
+#hidden:隐层神经元数量
+
+tf.reset_default_graph()
+
+num_unroll=50
+
+
+
+
+
